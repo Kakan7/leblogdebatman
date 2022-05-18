77c400a12e45d61fb09c3a45c1e28158a1ca6f05
sheesh <a@a.fr>
2022-05-18 10:12:26 +0200

    Ajout page de profil

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -14,5 +15,13 @@ class MainController extends AbstractController
     public function home(): Response
     {
         return $this->render('main/home.html.twig');
+    }
+
+    //Controller de la page d'accueil
+    #[Route('/mon-profil/', name: 'profil')]
+    #[IsGranted('ROLE_USER')]
+    public function profil(): Response
+    {
+        return $this->render('main/profil.html.twig');
     }
 }
